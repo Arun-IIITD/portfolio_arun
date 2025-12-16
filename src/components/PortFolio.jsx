0723f59f2339529efa6c8f4ddcfd6b55d80569ae
Arun Yadav <ay20552291@gmail.com>
2025-12-16 10:32:25 +0530

    wallet

--- src/components/PortFolio.jsx
+++ src/components/PortFolio.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 //import { FaShoppingCart } from "react-icons/fa";
 import { FaShoppingCart, FaTasks } from "react-icons/fa";
+import { FaWallet } from "react-icons/fa";
+
 
 function PortFolio() {
   const cardItem = [
@@ -11,6 +13,8 @@ function PortFolio() {
       demoLink: "https://to-do-frontend-rosy.vercel.app/", // update if needed
       sourceLink: "https://github.com/Arun-IIITD/todo_app",
     },
+
+
     {
       id: 2,
       logo: <FaShoppingCart className="text-6xl text-green-500" />,
@@ -18,6 +22,18 @@ function PortFolio() {
       demoLink: "https://qkart-qd3l.vercel.app/",
       sourceLink: "https://github.com/Arun-IIITD/QKART",
     },
+
+
+     {
+      id: 3,
+      logo: <FaWallet className="text-6xl text-green-500" />,
+      name: "WALLET_TRACKER",
+      demoLink: "https://wallet-tracker-seven-rho.vercel.app/",
+      sourceLink: "https://github.com/Arun-IIITD/wallet_tracker",
+    },
+
+
+
   ];
 
   return (
